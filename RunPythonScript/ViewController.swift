0b01f7646391b84c35899278ab0a33967b797462
本間 和尊 <honmakazutaka@team-lab.com>
2017-09-05 20:25:32 +0900

    Initial Commit

---
+++ RunPythonScript/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RunPythonScript
+//
+//  Created by 本間 和尊 on 2017/09/05.
+//  Copyright © 2017 本間 和尊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
